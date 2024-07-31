672f41c2504c577307e8b4d79a505de7a091670b
Kevin <msnrelleno1@hotmail.com>
2024-07-31 08:46:11 +0200

    subiendo primer commit prueba js

---
+++ JS/javascript.js
@@ -0,0 +1,87 @@
+// // alert("Hola mundo desde JavaScript");
+
+// // Variables
+// let nombre = "Kevin";
+// console.log(nombre);
+
+// let precio = 36;
+// console.log("El precio es: " + precio);
+// precio = precio * 1.1;
+// console.log("El precio actualizado es: " + precio);
+
+// // Las constantes no pueden cambiar de valor
+// const dni = "9393947A";
+// console.log(dni)
+// // dni = "34234234";
+// // console.log(dni)
+
+// let cantidad = 2;
+// let precioProducto = 46.5;
+// let total = cantidad * precioProducto;
+// console.log(total);
+
+// let nom = "Pepe";
+// let apellido = "García";
+// let nombreCompleto = nom + " " + apellido;
+// console.log(nombreCompleto);
+
+// //console.log((String("hola")) * 7)
+
+// // Booleano
+// let esFinde = false;
+// console.log(esFinde);   
+
+// // Operadores
+// let cantidad1 = 7;
+// let cantidad2 = 2;
+
+// // console.log(cantidad1 % cantidad2)
+// // console.log(cantidad1>cantidad2);
+
+// let planta1 = "cactus";
+// let planta2 = "cactus";
+
+// console.log(planta1 == planta2)
+
+// let estaSobrio = false;
+// let tieneCarne = true;
+// let puedeConducir = estaSobrio && tieneCarne;
+
+// console.log("¿Puede conducir? " + puedeConducir)
+
+// let esFinde = false;
+// let esVerano = true;
+
+// let voyDePaseo = esFinde || esVerano;
+// console.log("¿Me voy de paseo? " + voyDePaseo)
+
+// let texto = "En un lugar de la Mancha...";
+// console.log(texto.length)
+
+// console.log(texto.toUpperCase());
+
+
+// EJERCICIOS
+// declarar una variable para guardar un nombre
+let nombre = "Fulanito"
+
+// declarar variable para guardar un apellido
+let apellidos = "Fernandez"
+
+// declarar una variable para la edad
+let edad = 24
+
+// mostrar po consola una frase: Fulanito Fernandez tiene tantos años
+console.log(nombre + " " + apellidos + " " + "tiene "+ edad + " años")
+
+
+
+
+
+
+
+
+
+
+
+
